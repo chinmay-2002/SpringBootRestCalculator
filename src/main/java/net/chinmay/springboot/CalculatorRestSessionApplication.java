3625c34236208bd22ec4be15544504dbe1dda207
Chinmay <chinmaymp007@gmail.com>
2024-03-30 12:20:04 +0530

    Addsed

---
+++ src/main/java/net/chinmay/springboot/CalculatorRestSessionApplication.java
@@ -0,0 +1,13 @@
+package net.chinmay.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorRestSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorRestSessionApplication.class, args);
+	}
+
+}
